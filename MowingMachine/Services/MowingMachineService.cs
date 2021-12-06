7079894f84e68d2ad949a424db9dd4e7b47b158f
Solomon Ezenwa Kanu-Agha <kanu-agha@adremes.com>
2021-12-06 08:51:37 +0100

    x

--- MowingMachine/Services/MowingMachineService.cs
+++ MowingMachine/Services/MowingMachineService.cs
@@ -115,7 +115,7 @@ namespace MowingMachine.Services
             if (offset.CompareTo(new Offset(0, -1)))
                 return MoveDirection.Bottom;
             if (offset.CompareTo(new Offset(-1, 0)))
-                return MoveDirection.Left;
+                return MoveDirection.Bottom;
 
             throw new ArgumentException("Offset was beyond");
         }
