c8226886300efe60d8aa3255cdc1de4cefbb3441
Solomon Ezenwa Kanu-Agha <SolomonRosemiteProjects@gmail.com>
2021-12-10 15:16:39 +0100

    also state

---
+++ MowingMachine/Common/FieldComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using MowingMachine.Models;
+
+namespace MowingMachine.Common
+{
+    public class FieldComparer : IEqualityComparer<Field>
+    {
+        public bool Equals(Field x, Field y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Offset.X == y.Offset.X && x.Offset.Y == y.Offset.Y;
+        }
+
+        public int GetHashCode(Field obj)
+        {
+            return HashCode.Combine(obj.Offset.X, obj.Offset.Y);
+        }
+    }
+}
